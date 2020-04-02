53f4fd77651c04a0a7f5436c0efb39983259395a
Aleksandr Tolstoy <aleksandr13tolstoy@gmail.com>
2020-04-03 00:18:54 +0300

    from sketch

--- indicators.py
+++ indicators.py
@@ -3,19 +3,17 @@ import yfinance as yf
 import numpy as np
 import matplotlib.pyplot as plt
 
-ibm = yf.download('IBM', '2016-01-01')
-# Скорректированая цена закрытия`
-daily_close = ibm['Adj Close']
+ibm = yf.download('AAPL', '2020-01-01')
+
+# Adjusted closing price
+daily_close = ibm[['Adj Close']]
 print(daily_close)
-# Дневная доходность
-# daily_pct_change = daily_close.pct_change()
-#
-# # Заменить NA значения на 0
-# daily_pct_change.fillna(0, inplace=True)
-#
-# print(daily_pct_change.head())
-#
-# # Дневная лог доходность
-# daily_log_returns = np.log(daily_close.pct_change()+1)
-#
-# print(daily_log_returns.head())
+
+# Daily yield
+daily_pct_change = daily_close.pct_change()
+daily_pct_change.fillna(0, inplace=True)
+print(daily_pct_change)
+
+# Daily log yield
+daily_log_returns = np.log(daily_close.pct_change()+1)
+print(daily_log_returns)

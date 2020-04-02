cc680a2ae442f1886d8018d7d7d7f9ccf0dd3c9a
Aleksandr Tolstoy <aleksandr13tolstoy@gmail.com>
2020-04-03 00:32:21 +0300

    more outline

--- indicators.py
+++ indicators.py
@@ -3,17 +3,29 @@ import yfinance as yf
 import numpy as np
 import matplotlib.pyplot as plt
 
-ibm = yf.download('AAPL', '2020-01-01')
+stock = yf.download('AAPL', '2019-01-01')
 
 # Adjusted closing price
-daily_close = ibm[['Adj Close']]
-print(daily_close)
+daily_close = stock[['Adj Close']]
 
 # Daily yield
 daily_pct_change = daily_close.pct_change()
 daily_pct_change.fillna(0, inplace=True)
-print(daily_pct_change)
 
 # Daily log yield
+# Allows you to better understand and explore changes over time
 daily_log_returns = np.log(daily_close.pct_change()+1)
-print(daily_log_returns)
+
+
+# Take stock values for the last business day of the month
+monthly = stock.resample('BM').apply(lambda x: x[-1])
+
+# Monthly yield
+print(monthly.pct_change().tail())
+
+# Recalculate the stock by quarters and take the average value for the quarter
+quarter = stock.resample("4M").mean()
+
+# Quarterly yield
+print(quarter.pct_change().tail())
+

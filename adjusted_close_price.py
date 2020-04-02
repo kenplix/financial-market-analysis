89ff4f24426367080b38d2089a6e427bfef533d3
Aleksandr Tolstoy <aleksandr13tolstoy@gmail.com>
2020-04-02 16:56:02 +0300

    v0.1

--- adjusted_close_price.py
+++ adjusted_close_price.py
@@ -1,10 +1,24 @@
 import yfinance as yf
 import matplotlib.pyplot as plt
+import pandas as pd
 
-# Get the data for the stock AAPL
-data = yf.download('AAPL','2019-04-01','2020-04-01')
+# Identify publicly traded shares of a particular stock on a particular stock market
+tickers_list = ['AAPL', 'WMT', 'IBM', 'MU', 'BA', 'AXP']
+data = pd.DataFrame(columns=tickers_list)
 
-# Plot the close price of the AAPL
-print(data)
-data['Adj Close'].plot()
+# Fetch the data
+for ticker in tickers_list:
+    data[ticker] = yf.download(ticker, '2019-04-01', '2020-04-01')['Adj Close']
+
+# Plot all the close prices
+aligned_percentage = data.pct_change() + 1
+cumulative_product = aligned_percentage.cumprod()
+
+cumulative_product.plot(figsize=(10, 7))
+plt.legend()
+plt.title("Adjusted Close Price", fontsize=16)
+plt.ylabel('Price', fontsize=14)
+plt.xlabel('Year', fontsize=14)
+plt.grid(which="major", color='k', linestyle='-.', linewidth=0.5)
 plt.show()
+

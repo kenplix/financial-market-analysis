dbc29bc8a59f07768a1f41a3cebf686f7fe3424a
Aleksandr Tolstoy <aleksandr13tolstoy@gmail.com>
2020-04-03 16:02:42 +0300

    create Vizualizator

--- indicators.py
+++ indicators.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+
 import yfinance as yf
 import numpy as np
 import matplotlib.pyplot as plt
@@ -12,6 +14,7 @@ modes = [DAILY, MONTHLY, QUARTER]
 
 def profitability(stocks: pd.DataFrame, mode: str = DAILY, log: bool = False) -> pd.DataFrame:
     """
+    :param stocks : Downloaded data about the stock parameter
     :param mode: Displays what data to collect
     :param log: Allows you to better understand and explore changes over time
     :return: Profitability on the collected data
@@ -35,21 +38,27 @@ def profitability(stocks: pd.DataFrame, mode: str = DAILY, log: bool = False) ->
 def cumulative_profitability(prof: pd.DataFrame) -> pd.DataFrame:
     return (1 + prof).cumprod()
 
-def draw_distribution(prof: pd.DataFrame) -> None:
-    prof.hist(bins=50, sharex=True, figsize=(20, 8))
-    plt.show()
 
-def draw_scatter_matrix(prof: pd.DataFrame) -> None:
-    scatter_matrix(prof, diagonal='kde', alpha=0.1, figsize=(20, 20))
-    plt.show()
+@dataclass
+class Visualizator:
+    profitability: pd.DataFrame
 
-def draw_cumulative(prof: pd.DataFrame) -> None:
-    cumulative_profitability(prof).plot(figsize=(10, 7))
-    plt.legend()
-    plt.ylabel('Price', fontsize=14)
-    plt.xlabel('Year', fontsize=14)
-    plt.grid(which="major", color='k', linestyle='-.', linewidth=0.5)
-    plt.show()
+    def distribution(self) -> None:
+        self.profitability.hist(bins=50, sharex=True, figsize=(20, 8))
+        plt.show()
+
+    def scatter(self) -> None:
+        scatter_matrix(self.profitability, diagonal='kde', alpha=0.1, figsize=(20, 20))
+        plt.show()
+
+    def cumulative(self) -> None:
+        cumulative_profitability(self.profitability).plot(figsize=(10, 7))
+        plt.legend()
+        plt.ylabel('Price', fontsize=14)
+        plt.xlabel('Year', fontsize=14)
+        plt.grid(which="major", color='k', linestyle='-.', linewidth=0.5)
+        plt.show()
+
 
 def main():
     parameter = select_parameter()
@@ -58,9 +67,11 @@ def main():
     data = fetch_data(parameter, tickers_list, start_date)
     prof = profitability(stocks=data, mode=DAILY)
 
-    draw_distribution(prof)
-    draw_scatter_matrix(prof)
-    draw_cumulative(prof)
+    v = Visualizator(prof)
+    v.distribution()
+    v.scatter()
+    v.cumulative()
+
 
 if __name__ == '__main__':
     main()
